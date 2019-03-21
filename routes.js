17842016a53f7378c66d6140f3c83d7a167005c4
sahilkindarle <sahilkindarle212@gmail.com>
2019-03-21 17:19:26 +0530

    3 routes added

--- routes.js
+++ routes.js
@@ -3,8 +3,17 @@
 let router = require('express').Router()
 
 router.get('/', (req, res) => {
-	res.render('index',{name:'Home'})
+	res.render('index',{name:'Home | Numerio'})
 })
+
+router.get('/contact', (req, res) => {
+	res.render('contact',{name:'Contact | Numerio'})
+})
+
+router.get('/marg', (req, res) => {
+	res.render('marg',{name:'Marg | Numerio'})
+})
+
 
 router.get('/about', (req, res) => {
 	res.render('about')
